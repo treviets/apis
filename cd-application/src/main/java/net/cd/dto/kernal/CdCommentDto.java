e95f57252d393c0b726cf4d933422764ee57c454
CaoHongVu <vuch@proteusion.com>
2017-12-21 00:00:08 +0700

    update code to newtest

--- cd-application/src/main/java/net/cd/dto/kernal/CdCommentDto.java
+++ cd-application/src/main/java/net/cd/dto/kernal/CdCommentDto.java
@@ -1,9 +1,16 @@
 package net.cd.dto.kernal;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class CdCommentDto {
 	private Integer id;
 	private CdKMemberDto author;
 	private String comment;
+	private String made;
+	private String revised;
+	@JsonIgnore
+	private String purged;
+	
 	public Integer getId() {
 		return id;
 	}
@@ -22,6 +29,27 @@ public class CdCommentDto {
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
+	public String getMade() {
+		return made;
+	}
+	public void setMade(String made) {
+		this.made = made;
+	}
+	public String getRevised() {
+		return revised;
+	}
+	public void setRevised(String revised) {
+		this.revised = revised;
+	}
+	public String getPurged() {
+		return purged;
+	}
+	public void setPurged(String purged) {
+		this.purged = purged;
+	}
+	
+	
+	
 	
 	
 }
